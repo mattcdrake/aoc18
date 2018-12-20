5bc64b512b262adc7ce1d9a0c5f6be640652d367
Matt Drake <drake.mattc@gmail.com>
2018-12-20 16:37:13 -0500

    Cleaning more

--- README.md
+++
@@ -1,7 +0,0 @@
-# AOC18
-
-My solutions for the 2018 Advent of Code competition, implemented in Java.
-
-## TODO
-
-- Add testing
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        // TODO: Add a menu system
-        Solutions solutions = new Solutions();
-    }
-}
