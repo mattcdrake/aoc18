2b532c537a7df7b7ec9b97b24d96f751ded75470
Matt Drake <drake.mattc@gmail.com>
2019-03-11 18:56:39 -0400

    Setting up repo in another dev environment.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-    "extends": ["airbnb-base"],
-    "rules": {
-        "no-plusplus": "off",
-        "arrow-body-style": ["error", "as-needed"],
-        "operator-linebreak": [2, "after"]
-    }
+  extends: ['airbnb-base'],
+  rules: {
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'arrow-body-style': ['error', 'as-needed'],
+    'operator-linebreak': [2, 'after'],
+  },
 };
--- app.js
+++ app.js
@@ -29,4 +29,6 @@ app.get('/solutions/:puzzleId', (req, res) => {
   }
 });
 
-app.listen(3000, () => {});
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+});
